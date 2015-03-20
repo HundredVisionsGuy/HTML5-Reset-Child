f01bc2c7ba921e70394f9ed741d971b0569e6e63
hundredvisionsguy <winikkc@hsd.k12.or.us>
2015-03-20 09:38:24 -0700

    added bootstrap files

--- functions.php
+++ functions.php
@@ -10,6 +10,7 @@
 		wp_enqueue_style('parent-style', 
 		get_template_directory_uri() . '/reset.css' );
 		
+		// Load child theme styles
 		wp_enqueue_style('child-style',
 		get_stylesheet_directory_uri() . '/style.css',
 		array('parent-style')
